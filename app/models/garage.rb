0b66953e2637d86338c2bc1598beefa9e899b7bb
mathieu ralambotsirofo <m.ralambotsirofo@gmail.com>
2023-02-27 17:58:40 +0100

    models with relation 1:N and N:N

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,3 +1,4 @@
 class Garage < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    has_many :garages
+    has_many :bookings
+
+    validates :name, :email, presence: true
+
 end
