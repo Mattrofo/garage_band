a3a6ba26d77fe09cce7776ce14e73f95bcee47ed
jernito naisso <jernito.naisso@gmail.com>
2023-03-01 11:48:50 +0100

    créer le fichier .env

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -3,4 +3,7 @@ class Garage < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :price, :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
