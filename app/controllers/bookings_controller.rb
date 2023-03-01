5cc0f2e8e35ca4f1c4bae2e2738064fd397405eb
Eva Tchidemian <e.tchidemian@yahoo.fr>
2023-03-01 12:15:59 +0100

    tout et rien

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,24 @@
+class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @garage = Garage.find(params[:garage_id])
+    @booking.garage = @garage
+    if @booking.save
+      flash[:success] = "Réservation créée avec succès !"
+      redirect_to root_path
+    else
+      render :action => 'new'
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :user_id)
+  end
+end
