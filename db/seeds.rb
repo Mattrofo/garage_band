b1185b4e64abc6b1545016ba18fe58f6cc7d35ae
mathieu ralambotsirofo <m.ralambotsirofo@gmail.com>
2023-03-02 14:02:52 +0100

    structure index with cards responsive

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,5 +20,9 @@ Garage.create(titre: "le manoir", address: "13 rue du chemin vert, Boulogne-sur-
 Garage.create(titre: "la place du chef", address: "3 avenue de la serane, Marseille", price: 200, user: user2)
 Garage.create(titre: "big place", address: "2 avenue des saules, Lille", price: 300, user: user3)
 Garage.create(titre: "la finesse incarnée", address: "12 boulevard vauban, Lille", price: 450, user: user4)
+Garage.create(titre: "le poulpe", address: "568 rue de l'avenir, lille", price: 100, user: user1)
+Garage.create(titre: "la place du chicour", address: "17 avenue du général de gaulle, lille", price: 400, user: user2)
+Garage.create(titre: "le parking adoré", address: "2 rue du terroir, Lille", price: 300, user: user3)
+Garage.create(titre: "l'unique", address: "17 boulevard vauban, Lille", price: 750, user: user4)
 
 p "------ #{Garage.count} garage created ----------"
